8ca0e67b410f08a680c6f792ff518e3b99011341
drandazzo <david.randazzo@gmail.com>
2014-06-22 11:03:10 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Comments included
+## First function makeCacheMatrix: this creates a special matrix object that caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # set the stored inverse value to NULL to begin
+  invm <- NULL
+  
+  # set the value of the matrix here
+  setm <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  # get the value of the matrix here
+  getv <- function() x
+  
+  # set the value of the inverse of the matrix here
+  setinvm <- function(solve) invm <<- solve
+  
+  # pull value of invers of matrix here  
+  getinvm <- function() invm
+  
+  # return a list with the results here
+  list(setm = setm, getv = getv,
+       setinvm = setinvm,
+       getinvm = getinvm)
+  
 }
 
+## Second function casheSolve: calculates the inverse of the special matrix unless it has 
+## already been calculated. if so, then it returns the calculated inverse, skipping the inverting step.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  # here is the check to see if the inverse has already been calculated
+  z <- x$getinvm()
+  if(!is.null(z)) {
+    message("getting cached data")
+    return(z)
+  }
+  # if it does not find the cashed record, calculate it
+  b <- x$getinvm()
+  
+  # calcuate the inverse here
+  z <- solve(b, ...)
+  
+  # set the cache for use
+  x$setinvm(z)
+  
+  # return the inverse
+  z
 }
